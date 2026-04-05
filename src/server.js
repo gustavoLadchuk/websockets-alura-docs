3d3585dd20e424fc7355b9b47ad0d71412c87db9
gustavoLadchuk <gusta.ladchuk@gmail.com>
2026-04-05 20:31:22 -0300

    Criando e conectando o servidor do socket.io

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,5 +12,13 @@ const publicDir = path.join(currentPath, "../..", "public");
 
 app.use(express.static(publicDir));
 
-app.listen(PORT, () => console.log(`Servidor inicializado na porta ${PORT}`));
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, () => console.log(`Servidor inicializado na porta ${PORT}`));
+
+const io = new Server(httpServer)
+
+io.on("connection", () => {
+    console.log("Um cliente se conectou ao servidor")
+})
 
