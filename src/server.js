ac3d40fc7a8c86750e853fb735dfce7b5877d673
gustavoLadchuk <gusta.ladchuk@gmail.com>
2026-04-05 20:15:22 -0300

    Setup do servidor

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import url from "url";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const currentPath = url.fileURLToPath(import.meta.url);
+const publicDir = path.join(currentPath, "../..", "public");
+
+app.use(express.static(publicDir));
+
+app.listen(PORT, () => console.log(`Servidor inicializado na porta ${PORT}`));
+
